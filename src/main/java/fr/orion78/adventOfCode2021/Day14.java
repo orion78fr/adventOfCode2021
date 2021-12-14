950b37ea711963291c39c65dd2bc76ffbc7ea640
Guillaume Turchini <orion+github@orion78.fr>
2021-12-14 11:03:05 +0100

    Day 14 part 2 with memoization works (added for completeness sake)

--- src/main/java/fr/orion78/adventOfCode2021/Day14.java
+++ src/main/java/fr/orion78/adventOfCode2021/Day14.java
@@ -25,6 +25,9 @@ public class Day14 {
 
     }
 
+    public record MemKey(Pair p, int steps) {
+    }
+
     @Part1
     public static long part1(Input input) {
         List<Character> l = new ArrayList<>();
@@ -45,51 +48,71 @@ public class Day14 {
 
     @Part1(optLevel = 1)
     public static long part1v2(Input input) {
-        Map<Pair, Long> m = new HashMap<>();
-
-        for (int i = 0; i + 1 < input.init.length(); i++) {
-            m.compute(new Pair(input.init.charAt(i), input.init.charAt(i + 1)), (k, v) -> v == null ? 1 : v + 1);
-        }
-
-        for (int step = 0; step < 10; step++) {
-            Map<Pair, Long> m2 = new HashMap<>();
-            for (Map.Entry<Pair, Long> e : m.entrySet()) {
-                Pair p = e.getKey();
-                char c = input.transforms.get(p);
-
-                m2.compute(new Pair(p.l, c), (k, v) -> v == null ? e.getValue() : v + e.getValue());
-                m2.compute(new Pair(c, p.r), (k, v) -> v == null ? e.getValue() : v + e.getValue());
-            }
-            m = m2;
-        }
-
-        Map<Character, Long> res = new HashMap<>();
-
-        for (Map.Entry<Pair, Long> e : m.entrySet()) {
-            Pair p = e.getKey();
-
-            res.compute(p.l, (k, v) -> v == null ? e.getValue() : v + e.getValue());
-            // Don't add the right char, or it will be counted twice
-        }
-
-        // Don't forget the rightmost char, which doesn't change
-        res.compute(input.init.charAt(input.init.length() - 1), (k, v) -> v == null ? 1 : v + 1);
-
-        return res.values().stream().max(Comparator.naturalOrder()).get() - res.values().stream().min(Comparator.naturalOrder()).get();
+        return polymerize(input, 10);
     }
 
     // part2v1 and part2v2 did not finish
     // were recursive version of part1
 
+    @Part2(optLevel = 1)
+    public static long memoizedPart2v2(Input input) {
+        Map<MemKey, Map<Character, Long>> memo = new HashMap<>();
+        Map<Character, Long> m = new HashMap<>();
+
+        for (int i = 0; i + 1 < input.init.length(); i++) {
+            merge(m, visit(memo, input, input.init.charAt(i), input.init.charAt(i + 1), 40));
+        }
+
+        for (int i = 0; i < input.init.length(); i++) {
+            m.compute(input.init.charAt(i), (k, v) -> v == null ? 1 : v + 1);
+        }
+
+        return m.values().stream().max(Comparator.naturalOrder()).get() - m.values().stream().min(Comparator.naturalOrder()).get();
+    }
+
+    public static Map<Character, Long> visit(Map<MemKey, Map<Character, Long>> memo, Input input, char l, char r, int steps) {
+        if (steps == 0) {
+            return Collections.emptyMap();
+        }
+
+        MemKey memKey = new MemKey(new Pair(l, r), steps);
+        if (memo.containsKey(memKey)) {
+            return memo.get(memKey);
+        }
+
+        char c = input.getTransform(l, r);
+
+        Map<Character, Long> m = new HashMap<>();
+
+        merge(m, visit(memo, input, l, c, steps - 1));
+        merge(m, visit(memo, input, c, r, steps - 1));
+
+        m.compute(c, (k, v) -> v == null ? 1 : v + 1);
+
+        memo.put(memKey, m);
+
+        return m;
+    }
+
+    public static void merge(Map<Character, Long> l, Map<Character, Long> r) {
+        for (Map.Entry<Character, Long> e : r.entrySet()) {
+            l.put(e.getKey(), l.getOrDefault(e.getKey(), 0L) + e.getValue());
+        }
+    }
+
     @Part2(optLevel = 2)
     public static long part2v3(Input input) {
+        return polymerize(input, 40);
+    }
+
+    public static long polymerize(Input input, int steps) {
         Map<Pair, Long> m = new HashMap<>();
 
         for (int i = 0; i + 1 < input.init.length(); i++) {
             m.compute(new Pair(input.init.charAt(i), input.init.charAt(i + 1)), (k, v) -> v == null ? 1 : v + 1);
         }
 
-        for (int step = 0; step < 40; step++) {
+        for (int step = 0; step < steps; step++) {
             Map<Pair, Long> m2 = new HashMap<>();
             for (Map.Entry<Pair, Long> e : m.entrySet()) {
                 Pair p = e.getKey();
@@ -140,6 +163,7 @@ public class Day14 {
         Input l = parse(br.lines());
 
         System.out.println(part1v2(l));
+        System.out.println(memoizedPart2v2(l));
         System.out.println(part2v3(l));
     }
 }
